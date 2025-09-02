cadb016850c69af72b7f27fba9f9ff900253f88e
Andrei Zanozin <andrei-zanozin@Andreis-MacBook-Pro.local>
2025-09-03 00:52:50 +0200

    Initial commit.

---
+++ src/main/java/io/zanozin/rmi_ai/RmiAiApplication.java
@@ -0,0 +1,13 @@
+package io.zanozin.rmi_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmiAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RmiAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/zanozin/rmi_ai/RmiAiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.zanozin.rmi_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RmiAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
