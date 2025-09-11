7c79e81f56ddb1bf81fc61286a9368d1b2c1d9c2
Andrei Zanozin <andrei-zanozin@Andreis-MacBook-Pro.local>
2025-09-12 00:04:38 +0200

    Added container structure.

--- src/main/java/io/zanozin/rmi_ai/domain/entity/ContainerRawMaterial.java
+++ src/main/java/io/zanozin/rmi_ai/domain/entity/ContainerRawMaterial.java
@@ -1,4 +1,30 @@
 package io.zanozin.rmi_ai.domain.entity;
 
-public class ContainerRawMatrial {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class ContainerRawMaterial {
+
+    @Id
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "container_id")
+    private Container container;
+
+    private String name;
+
+    @OneToMany(
+            mappedBy = "containerRawMaterial",
+            orphanRemoval = true,
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+    )
+    private List<ContainerOperatingWeight> operatingWeights;
 }
--- src/main/java/io/zanozin/rmi_ai/domain/entity/Kpi.java
+++ src/main/java/io/zanozin/rmi_ai/domain/entity/Kpi.java
@@ -1,4 +1,25 @@
 package io.zanozin.rmi_ai.domain.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@Setter
 public class Kpi {
+
+    @Id
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "container_operating_weight_id")
+    private ContainerOperatingWeight containerOperatingWeight;
+
+    /**
+     * Raw Material Impact, %.
+     */
+    private BigDecimal rmiPercent;
 }
